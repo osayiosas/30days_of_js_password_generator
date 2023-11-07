e78fca75ca271e2ae703bec31ba24c4126c34c05
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-11-07 13:26:41 +0100

    Day4 of 30days of js

---
+++ README.md
@@ -0,0 +1,3 @@
+# 30days_of_js_password_generator
+
+## to generate the password for strong authentication
---
+++ main/main.js
@@ -0,0 +1,28 @@
+const passwordBox = document.getElementById("password");
+const length = 13;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "#$%&'[]|~!^()*+,-./:;<=>?@";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPasswords() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passwordBox.value = password;
+}
+
+function copyPasswords() { 
+  passwordBox.select();
+  document.execCommand("copy");
+}
